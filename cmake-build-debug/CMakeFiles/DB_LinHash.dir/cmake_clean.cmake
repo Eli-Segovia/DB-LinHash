423f575023fec4208775a4ed8aaf0ee29de96506
Eli <elisego46@gmail.com>
2021-10-25 13:05:08 -0500

    Add config files
    * Add .gitignore
        - ignore main.cpp (only used for viewing implmentation locally)
    * Add CmakeLists.txt

---
+++ cmake-build-debug/CMakeFiles/DB_LinHash.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DB_LinHash.dir/main.cpp.o"
+  "DB_LinHash"
+  "DB_LinHash.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DB_LinHash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
