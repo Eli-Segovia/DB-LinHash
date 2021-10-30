b22267eefbe5ee67df1a366222ca17e9b35ad6fa
Eli <elisego46@gmail.com>
2021-10-30 16:09:36 -0500

    Add testcase

--- test/Counting.cpp
+++ test/Counting.cpp
@@ -4,85 +4,179 @@
 #include <gtest/gtest.h>
 #include "../lib/LinearHashing/LinearHashing.h"
 
-int pageSize = 5;
-int policy = 0;
-    TEST(Counting, Counting_100_0) {
-        const int limit = 100;
+struct helper{
+public:
+    int policy = 0;
+    int pageSize = 5;
+public:
+    void Policy(int a) {this->policy = a;}
+    void PageSize(int a) {this->pageSize = a;}
+};
 
-        LinearHashing ln(pageSize, policy);
-        for(int i = 0; i < limit; i++) {
-            ln.Insert(rand() % 100000 + 1);
-            EXPECT_EQ(i+1, ln.Count());
-        }
-        EXPECT_EQ(limit, ln.Count());
+helper h = helper();
+
+TEST(Counting, Counting_100_0) {
+    const int limit = 100;
+
+    LinearHashing ln(h.pageSize, h.policy);
+    for(int i = 0; i < limit; i++) {
+        ln.Insert(rand() % 100000 + 1);
+        EXPECT_EQ(i+1, ln.Count());
     }
+    EXPECT_EQ(limit, ln.Count());
+}
 
-    TEST(Counting, Counting_1000_0) {
-        const int limit = 1000;
+TEST(Counting, Counting_1000_0) {
+    const int limit = 1000;
 
-        LinearHashing ln(pageSize, policy);
-        for(int i = 0; i < limit; i++) {
-            ln.Insert(rand() % 100000 + 1);
-        }
-        EXPECT_EQ(limit, ln.Count());
+    LinearHashing ln(h.pageSize, h.policy);
+    for(int i = 0; i < limit; i++) {
+        ln.Insert(rand() % 100000 + 1);
     }
+    EXPECT_EQ(limit, ln.Count());
+}
 
-    TEST(Counting, Counting_10000_0) {
-        const int limit = 10000;
+TEST(Counting, Counting_10000_0) {
+    const int limit = 10000;
 
-        LinearHashing ln(pageSize, policy);
-        for(int i = 0; i < limit; i++) {
-            ln.Insert(rand() % 100000 + 1);
-        }
-        EXPECT_EQ(limit, ln.Count());
+    LinearHashing ln(h.pageSize, h.policy);
+    for(int i = 0; i < limit; i++) {
+        ln.Insert(rand() % 100000 + 1);
     }
+    EXPECT_EQ(limit, ln.Count());
+}
 
-//    TEST(Counting, Counting_100000_0) {
-//        const int limit = 100000;
-//
-//        LinearHashing ln(pageSize, policy);
-//        for(int i = 0; i < limit; i++) {
-//            ln.Insert(rand() % 1000000 + 1);
-//        }
-//        EXPECT_EQ(limit, ln.Count());
-//    }
+TEST(Counting, Counting_100000_0) {
+    const int limit = 100000;
 
-//    TEST(Counting, Counting_100_1) {
-//        const int limit = 100;
-//
-//        LinearHashing ln(5, policy);
-//        for(int i = 0; i < limit; i++) {
-//            ln.Insert(rand() % 100000 + 1);
-//        }
-//        EXPECT_EQ(limit, ln.Count());
-//    }
-//
-//    TEST(Counting, Counting_1000_1) {
-//        const int limit = 1000;
-//
-//        LinearHashing ln(5,1);
-//        for(int i = 0; i < limit; i++) {
-//            ln.Insert(rand() % 100000 + 1);
-//        }
-//        EXPECT_EQ(limit, ln.Count());
-//    }
-//
-//    TEST(Counting, Counting_10000_1) {
-//        const int limit = 10000;
-//
-//        LinearHashing ln(5,1);
-//        for(int i = 0; i < limit; i++) {
-//            ln.Insert(rand() % 100000 + 1);
-//        }
-//        EXPECT_EQ(limit, ln.Count());
-//    }
-//
-//    TEST(Counting, Counting_100000_1) {
-//        const int limit = 100000;
-//
-//        LinearHashing ln(5,1);
-//        for(int i = 0; i < limit; i++) {
-//            ln.Insert(rand() % 1000000 + 1);
-//        }
-//        EXPECT_EQ(limit, ln.Count());
-//    }
+    LinearHashing ln(h.pageSize, h.policy);
+    for(int i = 0; i < limit; i++) {
+        ln.Insert(rand() % 1000000 + 1);
+    }
+    EXPECT_EQ(limit, ln.Count());
+}
+
+
+TEST(Counting, Counting_100_1) {
+    h.Policy(1);
+    const int limit = 100;
+
+    LinearHashing ln(h.pageSize, h.policy);
+    for(int i = 0; i < limit; i++) {
+        ln.Insert(rand() % 100000 + 1);
+    }
+    EXPECT_EQ(limit, ln.Count());
+}
+
+TEST(Counting, Counting_1000_1) {
+    const int limit = 1000;
+
+    LinearHashing ln(h.pageSize, h.policy);
+    for(int i = 0; i < limit; i++) {
+        ln.Insert(rand() % 100000 + 1);
+    }
+    EXPECT_EQ(limit, ln.Count());
+}
+
+TEST(Counting, Counting_10000_1) {
+    const int limit = 10000;
+
+    LinearHashing ln(h.pageSize,h.pageSize);
+    for(int i = 0; i < limit; i++) {
+        ln.Insert(rand() % 100000 + 1);
+    }
+    EXPECT_EQ(limit, ln.Count());
+}
+
+TEST(Counting, Counting_100000_1) {
+    const int limit = 100000;
+
+    LinearHashing ln(h.pageSize,h.policy);
+    for(int i = 0; i < limit; i++) {
+        ln.Insert(rand() % 1000000 + 1);
+    }
+    EXPECT_EQ(limit, ln.Count());
+}
+
+
+TEST(Counting, Counting_100_2) {
+    h.Policy(2);
+    const int limit = 100;
+
+    LinearHashing ln(h.pageSize, h.policy);
+    for(int i = 0; i < limit; i++) {
+        ln.Insert(rand() % 100000 + 1);
+    }
+    EXPECT_EQ(limit, ln.Count());
+}
+
+TEST(Counting, Counting_1000_2) {
+    const int limit = 1000;
+
+    LinearHashing ln(h.pageSize,h.policy);
+    for(int i = 0; i < limit; i++) {
+        ln.Insert(rand() % 100000 + 1);
+    }
+    EXPECT_EQ(limit, ln.Count());
+}
+
+TEST(Counting, Counting_10000_2) {
+    const int limit = 10000;
+
+    LinearHashing ln(h.pageSize,h.policy);
+    for(int i = 0; i < limit; i++) {
+        ln.Insert(rand() % 100000 + 1);
+    }
+    EXPECT_EQ(limit, ln.Count());
+}
+
+TEST(Counting, Counting_100000_2) {
+    const int limit = 100000;
+
+    LinearHashing ln(h.pageSize,h.policy);
+    for(int i = 0; i < limit; i++) {
+        ln.Insert(rand() % 1000000 + 1);
+    }
+    EXPECT_EQ(limit, ln.Count());
+}
+
+TEST(Counting, Counting_100_3) {
+    h.Policy(3);
+    const int limit = 100;
+
+    LinearHashing ln(h.pageSize, h.policy);
+    for(int i = 0; i < limit; i++) {
+        ln.Insert(rand() % 100000 + 1);
+    }
+    EXPECT_EQ(limit, ln.Count());
+}
+
+TEST(Counting, Counting_1000_3) {
+    const int limit = 1000;
+
+    LinearHashing ln(h.pageSize,h.policy);
+    for(int i = 0; i < limit; i++) {
+        ln.Insert(rand() % 100000 + 1);
+    }
+    EXPECT_EQ(limit, ln.Count());
+}
+
+TEST(Counting, Counting_10000_3) {
+    const int limit = 10000;
+
+    LinearHashing ln(h.pageSize,h.policy);
+    for(int i = 0; i < limit; i++) {
+        ln.Insert(rand() % 100000 + 1);
+    }
+    EXPECT_EQ(limit, ln.Count());
+}
+
+TEST(Counting, Counting_100000_3) {
+    const int limit = 100000;
+
+    LinearHashing ln(h.pageSize,h.policy);
+    for(int i = 0; i < limit; i++) {
+        ln.Insert(rand() % 1000000 + 1);
+    }
+    EXPECT_EQ(limit, ln.Count());
+}
